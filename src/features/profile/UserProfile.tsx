18090cc9b9524dd62795b5750c768aa2479e3c27
fanur56 <ilasovf@gmail.com>
2023-03-05 20:30:24 +0300

    refactor: added img to assests

--- src/features/profile/UserProfile.tsx
+++ src/features/profile/UserProfile.tsx
@@ -2,11 +2,10 @@ import { Avatar, Badge, styled } from '@mui/material'
 import Button from '@mui/material/Button'
 import Paper from '@mui/material/Paper'
 
-// import edit_photo from '../../assets/img/edit_photo.svg'
+import edit_photo from 'assets/img/edit_photo.svg'
 import edit_user_name from 'assets/img/edit_user_name.svg'
 import profile_logout from 'assets/img/profile_logout.svg'
-
-// import test_ava from '../../assets/img/test_ava.png'
+import test_ava from 'assets/img/test_ava.png'
 
 const SmallAvatar = styled(Avatar)(({ theme }) => ({
   width: 22,
@@ -31,9 +30,14 @@ export const UserProfile = () => {
       <Badge
         overlap="circular"
         anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        badgeContent={<SmallAvatar alt="Remy Sharp" src={''} />}
+        badgeContent={<SmallAvatar alt="Remy Sharp" src={edit_photo} />}
       >
-        <Avatar alt="UserName" style={{ width: '96px', height: '96px' }} src={''} sizes="small" />
+        <Avatar
+          alt="UserName"
+          style={{ width: '96px', height: '96px' }}
+          src={test_ava}
+          sizes="small"
+        />
       </Badge>
       <div>
         <span>Ivan</span>
