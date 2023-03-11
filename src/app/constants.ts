f64e74060354502b57d110718c842463aa4012aa
fanur56 <ilasovf@gmail.com>
2023-03-11 17:00:51 +0300

    refactor: added changeUserData to constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,10 +1,26 @@
 import { isFulfilled, isPending, isRejectedWithValue, isRejected } from '@reduxjs/toolkit'
 
 import { authMe, forgot, login, logOut, registerMe, setNewPassword } from 'features/auth'
+import { changeUserData } from 'features/profile/userProfile-slice'
 
-const pending = isPending(authMe, login, logOut, registerMe, forgot, setNewPassword)
-const fulfilled = isFulfilled(authMe, login, logOut, registerMe, forgot, setNewPassword)
-const rejected = isRejectedWithValue(login, logOut, registerMe, forgot, setNewPassword)
+const pending = isPending(authMe, login, logOut, registerMe, forgot, setNewPassword, changeUserData)
+const fulfilled = isFulfilled(
+  authMe,
+  login,
+  logOut,
+  registerMe,
+  forgot,
+  setNewPassword,
+  changeUserData
+)
+const rejected = isRejectedWithValue(
+  login,
+  logOut,
+  registerMe,
+  forgot,
+  setNewPassword,
+  changeUserData
+)
 
 const infoFulfilled = isFulfilled(logOut, forgot, registerMe, login, setNewPassword)
 const initApp = isRejected(authMe)
